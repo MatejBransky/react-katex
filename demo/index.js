9726e3f13853a3deb093914c02e524e6cfad8ff0
Matej Mazur <matej.mazur@mail.com>
2018-11-07 09:20:08 +0100

    update demo: use React's class

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import TeX from '../index';
@@ -12,59 +12,57 @@ const style = {
   border: '1px solid gray'
 };
 
-const App = () => {
-  const [value, setValue] = useState(_`y = k \cdot x + c`);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: _`y = k \cdot x + c`
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  const handleChange = event => {
-    setValue(event.target.value);
-  };
-  // TeX component is memoized so you can avoid rerendering
-  const renderError = useCallback(
-    err => (
-      <p>
-        <b>Custom</b> error message: {err.name}
-      </p>
-    ),
-    []
-  );
+  handleChange({ target: { value } }) {
+    this.setState({ value });
+  }
 
-  return (
-    <div style={style}>
-      <h1>Demo of react-katex</h1>
+  render() {
+    return (
+      <div style={style}>
+        <h1>Demo of react-katex</h1>
 
-      <h2>Dynamic formula</h2>
-      <input value={value} onChange={handleChange} />
-      {/* You can pass styles or classNames */}
-      <TeX
-        block
-        style={{ minHeight: '30px', textAlign: 'center', margin: '1rem 0' }}
-      >
-        {value}
-      </TeX>
+        <h2>Dynamic formula</h2>
+        <input value={this.state.value} onChange={this.handleChange} />
+        {/* You can pass styles or classNames */}
+        <TeX
+          block
+          style={{ minHeight: '30px', textAlign: 'center', margin: '1rem 0' }}
+        >
+          {this.state.value}
+        </TeX>
 
-      <h2>Inline math</h2>
-      <p>
-        This is an example of inline math (via <code>props.children</code>
-        ): <TeX>\int_0^\infty x^2 dx</TeX>.<br />
-        <i>
-          (you must use curly braces if you want to use chars like &quot;
-          {'{'}
-          &quot;, &quot;
-          {'}'}
-          &quot;)
-        </i>
-      </p>
-      <p>
-        And the next one is written via <code>props.math</code>:{' '}
-        <TeX math={_`\int_0^\infty x^2 dx`} />.
-      </p>
+        <h2>Inline math</h2>
+        <p>
+          This is an example of inline math (via <code>props.children</code>
+          ): <TeX>\int_0^\infty x^2 dx</TeX>.<br />
+          <i>
+            (you must use curly braces if you want to use chars like &quot;
+            {'{'}
+            &quot;, &quot;
+            {'}'}
+            &quot;)
+          </i>
+        </p>
+        <p>
+          And the next one is written via <code>props.math</code>:{' '}
+          <TeX math={_`\int_0^\infty x^2 dx`} />.
+        </p>
 
-      <h2>Block math</h2>
+        <h2>Block math</h2>
 
-      <span>
-        via <code>props.children</code>:
-      </span>
-      <TeX block>{_`
+        <span>
+          via <code>props.children</code>:
+        </span>
+        <TeX block>{_`
         \begin{pmatrix}
           1 & 0 & 0 \\
           0 & 1 & 0 \\
@@ -72,29 +70,38 @@ const App = () => {
         \end{pmatrix}
       `}</TeX>
 
-      <span>
-        via <code>props.math</code>:
-      </span>
-      <TeX math={_`\int_0^\infty x^2 dx`} block />
+        <span>
+          via <code>props.math</code>:
+        </span>
+        <TeX math={_`\int_0^\infty x^2 dx`} block />
 
-      <h2>Error handling</h2>
+        <h2>Error handling</h2>
 
-      <span>
-        <code>props.errorColor</code>:
-      </span>
-      <TeX errorColor="#cc0000" block>
-        \int_0^\infty x^2 dx \inta
-      </TeX>
+        <span>
+          <code>props.errorColor</code>:
+        </span>
+        <TeX errorColor="#cc0000" block>
+          \int_0^\infty x^2 dx \inta
+        </TeX>
 
-      <span>
-        <code>props.renderError</code>:
-      </span>
-      <TeX renderError={renderError} block>{_`\sum_{`}</TeX>
+        <span>
+          <code>props.renderError</code>:
+        </span>
+        <TeX renderError={renderError} block>{_`\sum_{`}</TeX>
 
-      <span>simple expression:</span>
-      <TeX block>123 + 5 = 128</TeX>
-    </div>
+        <span>simple expression:</span>
+        <TeX block>123 + 5 = 128</TeX>
+      </div>
+    );
+  }
+}
+
+function renderError(error) {
+  return (
+    <p>
+      <b>Custom</b> error message: {error.name}
+    </p>
   );
-};
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
